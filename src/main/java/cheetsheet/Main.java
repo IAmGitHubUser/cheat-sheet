45eecaadd3bf35cb3a4315ea2fd1c46ef8972d1d
Hardik Patel <hpatel@wsgc.com>
2018-08-19 14:51:06 -0700

    java

---
+++ src/main/java/cheetsheet/Main.java
@@ -0,0 +1,53 @@
+package cheetsheet;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] myIntArray = {1, 4, 6, 2, 8, 2, 8, 2, 8};
+        int myInt = 12345;
+        String myString = "My String";
+
+        // Get unique (distinct) element from Array
+        int[] uniqueNumbers = Arrays.stream(myIntArray).distinct().toArray();
+
+        // print an array
+        System.out.println("print an array " + Arrays.toString(uniqueNumbers));
+        // [1, 4, 6, 2, 8]
+
+        // Array filter
+        int[] arrayWithEachElementLessThanFive = Arrays.stream(myIntArray).filter(arrivalTime -> arrivalTime < 5).toArray();
+        System.out.println("Array filter" + Arrays.toString(arrayWithEachElementLessThanFive));
+        // [1, 4, 2, 2, 2]
+
+        // Maximum number for integer array
+        int maxOfAnArray = IntStream.of(myIntArray).summaryStatistics().getMax();
+        System.out.println("Maximum number for integer array" + maxOfAnArray);// 8
+
+        // Create Stream from array
+        Stream<int[]> stream = Stream.of(myIntArray);
+        IntStream stream2 = Arrays.stream(myIntArray);
+
+        // How to count a particular character in a string
+        long count = myString.chars().filter(ch -> ch == 't').count();
+        System.out.println("count chara:" + count); // 1
+
+        // Integer to String
+        StringBuilder stringBuilder = new StringBuilder(Integer.toString(myInt));
+        System.out.println("Integer to String: s" + stringBuilder);
+        // 12345 - string
+
+        // reversing string
+        String reverseString = stringBuilder.reverse().toString();
+        System.out.println("reversing string: " + reverseString);
+        // 54321 - string
+
+        // string to integer
+        int stringToInteger = Integer.parseInt(reverseString);
+        System.out.println("string to integer: " + stringToInteger);
+        // 54321- number
+
+    }
+}
